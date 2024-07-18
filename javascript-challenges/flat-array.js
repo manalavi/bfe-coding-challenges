920ff36e77ff70d753e9ad0b5bafcae87b2c4b8b
manalavi <vinay.manala@outlook.com>
2024-07-18 16:35:53 +0530

    Inital code commit

--- javascript-challenges/flat-array.js
+++ javascript-challenges/flat-array.js
@@ -4,39 +4,39 @@
  * Here is an example to illustrate
  */
 
-// const arr = [1, [2], [3, [4]]];
+const arr = [1, [2], [3, [4]]];
 
-// function flat(arr, depth = 1) {
-//   const result = [];
-//   const stack = [...arr.map((item) => [item, depth])];
+function flat(arr, depth = 1) {
+  const result = [];
+  const stack = [...arr.map((item) => [item, depth])];
 
-//   while (stack.length > 0) {
-//     const [top, depth] = stack.pop();
-//     if (Array.isArray(top) && depth > 0) {
-//       stack.push(...top.map((item) => [item, depth - 1]));
-//     } else {
-//       res.push(top);
-//     }
-//   }
-//   console.log(res.reverse());
+  while (stack.length > 0) {
+    const [top, depth] = stack.pop();
+    if (Array.isArray(top) && depth > 0) {
+      stack.push(...top.map((item) => [item, depth - 1]));
+    } else {
+      res.push(top);
+    }
+  }
+  console.log(res.reverse());
 
-//using recursion
-//   r(stack, result);
-// }
+  // using recursion
+  //   r(stack, result);
+}
 
-// function r(stack, res) {
-//   if (!stack.length) {
-//     console.log(res.reverse());
-//     return res;
-//   } else {
-//     const [top, depth] = stack.pop();
-//     if (Array.isArray(top) && depth > 0) {
-//       r([...stack, ...top.map((item) => [item, depth - 1])], res);
-//     } else {
-//       r(stack, [...res, top]);
-//     }
-//   }
-// }
+function r(stack, res) {
+  if (!stack.length) {
+    console.log(res.reverse());
+    return res;
+  } else {
+    const [top, depth] = stack.pop();
+    if (Array.isArray(top) && depth > 0) {
+      r([...stack, ...top.map((item) => [item, depth - 1])], res);
+    } else {
+      r(stack, [...res, top]);
+    }
+  }
+}
 
 // flat(arr);
 // flat(arr, 1);
--- javascript-challenges/implement-curryfunc.js
+++ javascript-challenges/implement-curryfunc.js
@@ -4,38 +4,37 @@
  * Here is an example
  */
 
-const join = (a, b, c) => {    
-    console.log(`${a}_${b}_${c}`)    
-    // return `${a}_${b}_${c}`
-}
- 
-// using closures 
-// function curry(fn){
-//     return function innerCurry(...args1){        
-//         if (args1.length >= fn.length){
-//             return fn(...args1)
-//         }     
-//         return function (...args2) {                
-//             return innerCurry(...args1.concat(...args2))
-//         }        
-//     }    
+const join = (a, b, c) => {
+  console.log(`${a}_${b}_${c}`);
+  // return `${a}_${b}_${c}`
+};
 
-// }
+// using closures
+function curry(fn) {
+  return function innerCurry(...args1) {
+    if (args1.length >= fn.length) {
+      return fn(...args1);
+    }
+    return function (...args2) {
+      return innerCurry(...args1.concat(...args2));
+    };
+  };
+}
 
 // using bind
-function curry(fn){
-    return function innerCurry(...args1){        
-        if (args1.length >= fn.length){
-            return fn(...args1)
-        }     
-        return innerCurry.bind(this, ...args1)  
-    }    
+function curry(fn) {
+  return function innerCurry(...args1) {
+    if (args1.length >= fn.length) {
+      return fn(...args1);
+    }
+    return innerCurry.bind(this, ...args1);
+  };
 }
 
-const curriedJoin = curry(join) 
+const curriedJoin = curry(join);
 
-curriedJoin(1, 2,3) // '1_2_3'
+// curriedJoin(1, 2, 3); // '1_2_3'
 
-curriedJoin(1)(2, 3) // '1_2_3'
+// curriedJoin(1)(2, 3); // '1_2_3'
 
-curriedJoin(1, 2)(3) // '1_2_3'
+// curriedJoin(1, 2)(3); // '1_2_3'
