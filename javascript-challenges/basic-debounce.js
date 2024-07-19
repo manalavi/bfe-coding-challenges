8a84f707efc107d527af1ec392c01302f3852662
manalavi <vinay.manala@outlook.com>
2024-07-19 11:32:28 +0530

    basic-debounce.js code

---
+++ javascript-challenges/basic-debounce.js
@@ -0,0 +1,24 @@
+/**
+ * In case you forgot, debounce(func, delay) will returned a debounced function, which delays the invoke
+ */
+
+function debounce(callback) {
+  let timer = null;
+
+  clearTimeout(timer);
+
+  return function (...args) {
+    let timer = setTimeout(() => {
+      callback.apply(this, args);
+      timer = null;
+    }, args);
+  };
+}
+
+const timerClick = (delay) => {
+  console.log(`clicked after ${delay}ms`);
+};
+
+const debouncedCall = debounce(timerClick);
+
+debouncedCall(2000);
